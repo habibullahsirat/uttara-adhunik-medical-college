99a242ce5d465688fc105c3108c591c89ae8122b
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-08-07 01:55:14 +0600

    History model added.

--- uttara-adhunik-medical-college-admin/lib/models/about/overview.ts
+++ uttara-adhunik-medical-college-admin/lib/models/about/overview.ts
@@ -134,6 +134,17 @@ const SustainabilitySchema = new Schema(
   { timestamps: true },
 );
 
+// History Section
+const HistorySchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    subtitle: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    image: { type: String, required: true },
+  },
+  { timestamps: true },
+);
+
 // Publication Section
 const PublicationSchema = new mongoose.Schema(
   {
@@ -519,6 +530,7 @@ delete mongoose.models.AboutOverviewHeroSection;
 delete mongoose.models.VisitSection;
 delete mongoose.models.AdmissionAidSchema;
 delete mongoose.models.SustainabilitySchema;
+delete mongoose.models.HistorySchema;
 
 // Other page
 delete mongoose.models.PublicationSection;
@@ -549,6 +561,10 @@ export const AdmissionAidSection =
 export const SustainabilitySection =
   mongoose.models.SustainabilitySection ||
   mongoose.model("SustainabilitySection", SustainabilitySchema);
+
+export const HistorySection =
+  mongoose.models.HistorySection ||
+  mongoose.model("HistorySection", HistorySchema);
 
 // Other page
 export const PublicationSection =

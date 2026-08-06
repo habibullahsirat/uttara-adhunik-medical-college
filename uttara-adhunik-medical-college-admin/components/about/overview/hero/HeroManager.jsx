8e4f5b28c3eb9767d0e5706b06cc5ea992c47d59
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-08-06 22:01:45 +0600

    Code updated.

--- uttara-adhunik-medical-college-admin/components/about/overview/hero/HeroManager.jsx
+++ uttara-adhunik-medical-college-admin/components/about/overview/hero/HeroManager.jsx
@@ -29,7 +29,7 @@ export default function HeroManager() {
     setIsDeleting(true);
     try {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/homepage/hero/${id}`,
+        `${process.env.NEXT_PUBLIC_API_URL}/api/about/overview/hero/${id}`,
         {
           method: "DELETE",
         },

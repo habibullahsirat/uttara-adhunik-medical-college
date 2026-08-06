3df9472eaca196cc9c8b944586ab8dd70a9b32ea
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-08-07 04:00:24 +0600

    Code added.

--- uttara-adhunik-medical-college-admin/app/api/about/vision-and-mission/route.ts
+++ uttara-adhunik-medical-college-admin/app/api/about/vision-and-mission/route.ts
@@ -1,14 +1,12 @@
 import { connectToDB } from "@/lib/connectToDB";
-import { export const VisionAndMissionSection =
- } from "@/lib/models/home-models";
+import { VisionAndMissionSection } from "@/lib/models/about/visionAndMission";
 import { NextResponse } from "next/server";
 
 const PUBLIC_URL = process.env.PUBLIC_URL || "";
 
 export async function GET() {
   await connectToDB();
-  const hero = await export const VisionAndMissionSection =
-.find();
+  const hero = await VisionAndMissionSection.find();
   const response = NextResponse.json(hero);
   // response.headers.set("Access-Control-Allow-Origin", "*");
   response.headers.set("Access-Control-Allow-Origin", PUBLIC_URL);
@@ -21,8 +19,7 @@ export async function POST(req: Request) {
 
     // Connect to the database
     await connectToDB();
-    await export const VisionAndMissionSection =
-.create(data);
+    await VisionAndMissionSection.create(data);
     return NextResponse.json({ message: "Data created" }, { status: 201 });
   } catch (error) {
     console.error("Error creating data:", error);

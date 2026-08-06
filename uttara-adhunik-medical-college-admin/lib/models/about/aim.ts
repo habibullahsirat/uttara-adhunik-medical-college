cd83cadea403d7bed6f9271ae3f3dfffb5f06cad
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-08-07 05:33:52 +0600

    Code updated.

--- uttara-adhunik-medical-college-admin/lib/models/about/aim.ts
+++ uttara-adhunik-medical-college-admin/lib/models/about/aim.ts
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 
+// Banner section
 const BannerSchema = new mongoose.Schema(
   {
     title1: { type: String, required: true, trim: true },
@@ -10,8 +11,23 @@ const BannerSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
+// Objective Section
+const ObjectiveSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    description1: { type: String, required: true, trim: true },
+    description2: { type: String, required: true, trim: true },
+  },
+  { timestamps: true },
+);
+
 delete mongoose.models.BannerSchema;
+delete mongoose.models.ObjectSchema;
 
 export const BannerSection =
   mongoose.models.BannerSection ||
   mongoose.model("BannerSection", BannerSchema);
+
+export const ObjectiveSection =
+  mongoose.models.ObjectiveSection ||
+  mongoose.model("ObjectiveSection", ObjectiveSchema);

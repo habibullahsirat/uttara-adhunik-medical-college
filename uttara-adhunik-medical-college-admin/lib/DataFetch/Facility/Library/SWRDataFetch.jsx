9e9f11a46815cbaf0e8b6e422c1bd9a673d70cca
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-08-08 07:46:41 +0600

    Code updated.

--- uttara-adhunik-medical-college-admin/lib/DataFetch/Facility/Library/SWRDataFetch.jsx
+++ uttara-adhunik-medical-college-admin/lib/DataFetch/Facility/Library/SWRDataFetch.jsx
@@ -6,7 +6,7 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const useLibraryData = () => {
   const { data, error, mutate } = useSWR(
-    `${API_URL}/api/facility/library`,
+    `${API_URL}/api/facility/library/about`,
     fetcher,
   );
   return { data, error, isLoading: !data && !error, mutate };

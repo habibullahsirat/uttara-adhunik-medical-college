2fca193c84ebcd310122e3facd7e1ab1e60c7ab1
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-08-10 02:18:41 +0600

    code updated.

--- uttara-adhunik-medical-college-admin/lib/DataFetch/Facility/Laboratory/SWRDataFetch.jsx
+++ uttara-adhunik-medical-college-admin/lib/DataFetch/Facility/Laboratory/SWRDataFetch.jsx
@@ -4,9 +4,9 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const useTrainingData = () => {
+export const useLaboratoryData = () => {
   const { data, error, mutate } = useSWR(
-    `${API_URL}/api/facility/training/about`,
+    `${API_URL}/api/facility/laboratory/about`,
     fetcher,
   );
   return { data, error, isLoading: !data && !error, mutate };
@@ -14,7 +14,7 @@ export const useTrainingData = () => {
 
 export const useFacilityData = () => {
   const { data, error, mutate } = useSWR(
-    `${API_URL}/api/facility/training/facility`,
+    `${API_URL}/api/facility/laboratory/facility`,
     fetcher,
   );
   return { data, error, isLoading: !data && !error, mutate };

078987ed94c2a3d4492c58067a60045e64b404a9
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-08-10 01:24:23 +0600

    hostel-models.ts added.

---
+++ uttara-adhunik-medical-college-admin/lib/models/facility/hostel-models.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+
+// Hostel Section
+const HostelSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Hostel Services section
+const ServicesSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+delete mongoose.models.SeminarSection;
+delete mongoose.models.WeeklyPresentationSection;
+delete mongoose.models.CollaborationSection;
+delete mongoose.models.ActivitySection;
+
+export const SeminarSection =
+  mongoose.models.SeminarSection ||
+  mongoose.model("SeminarSection", SeminarSchema);
+
+export const WeeklyPresentationSection =
+  mongoose.models.WeeklyPresentationSection ||
+  mongoose.model("WeeklyPresentationSection", WeeklyPresentationSchema);
+
+export const CollaborationSection =
+  mongoose.models.CollaborationSection ||
+  mongoose.model("CollaborationSection", CollaborationSchema);
+
+export const ActivitySection =
+  mongoose.models.ActivitySection ||
+  mongoose.model("ActivitySection", ActivitySchema);

a23543db51a6df8e4c57ab70c3bcb5924a00669c
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-08-16 02:17:07 +0600

    Letter corrected.

--- uttara-adhunik-medical-college-public/app/page.tsx
+++ uttara-adhunik-medical-college-public/app/page.tsx
@@ -3,15 +3,15 @@ import React from "react";
 import Hero from "@/components/Homepage/Hero";
 import StatStrip from "@/components/Homepage/StatStrip";
 import AboutUAMC from "@/components/Aboutpage/Overview/AboutSection";
-import NoticeAndPublication from "@/components/Homepage/NoticeAndPublication";
-import FindDepartment from "@/components/Homepage/FindDepartment";
-import Admission from "@/components/Homepage/Admission";
-import OurFacilities from "@/components/Homepage/OurFacilities";
-import PrincipalMessage from "@/components/Homepage/PrincipalMessage";
-import CampusLife from "@/components/Homepage/CampusLife";
-import AlumniEvents from "@/components/Homepage/AlumniEvents";
-import NewsSection from "@/components/Homepage/NewsSection";
-import Testimonials from "@/components/Homepage/Testimonials";
+import NoticeAndPublication from "@/components/homepage/NoticeAndPublication";
+import FindDepartment from "@/components/homepage/FindDepartment";
+import Admission from "@/components/homepage/Admission";
+import OurFacilities from "@/components/homepage/OurFacilities";
+import PrincipalMessage from "@/components/homepage/PrincipalMessage";
+import CampusLife from "@/components/homepage/CampusLife";
+import AlumniEvents from "@/components/homepage/AlumniEvents";
+import NewsSection from "@/components/homepage/NewsSection";
+import Testimonials from "@/components/homepage/Testimonials";
 
 const fontImport = `
   @import url('https://fonts.googleapis.com/css2?family=Bitter:wght@300;400;500;700&family=Inter:wght@400;500;600;700&display=swap');
